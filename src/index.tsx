b2c32515939a53dadad6048f27323e2a87da9a8e
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-10-03 23:46:34 +0300

    fix: fix react-app

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename="/react-gh-pages">
     <App />
   </BrowserRouter>
   // </React.StrictMode>
